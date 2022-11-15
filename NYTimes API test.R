2a40656f0039833fd22d05532d99ccefa287a89e
sparkay <sparkay@gmail.com>
2022-11-15 10:40:39 -0500

    removed my key and removed legacy code

--- NYTimes API test.R
+++ NYTimes API test.R
@@ -1,31 +1,17 @@
 #Sample NYTimes Article Search Script
-
-#library(httr2)
-
-#initialize the basic request - prob need to modify everything after the ?
-#req <- request("https://api.nytimes.com/svc/search/v2/articlesearch.json") %>%
-#        #add search terms & filters
-#        req_headers(q = "space", #search string
-#                    #pub_year = 2022, #filter on this year
-#                    #facet_filter="true", #restrict facet results to filter range
-#                    #facet_fields="pub_date", #return counts by day
-#                    'api-key'=Sys.getenv("NYTIMES_KEY")) #mykey
-                    
-#test_response <- req %>% req_perform()
-
-#teststr=paste0("https://api.nytimes.com/svc/search/v2/articlesearch.json","?",
-#               "q=","space","&","api-key=",Sys.getenv("NYTIMES_KEY"))
-#### httr2 didn't work for me - reverting to httr
+#### Simplified httr-only version
 library(httr)
 library(jsonlite)
 library(tidyverse)
 
+##### INPUTS #####
 ##### Change this string to change your search term 
 # Need to use + to string together separate words
 searchstr <- "space"
 searchyr <- 1880 #limit year to get < 1000 hits
+yourAPIkey <- "" #goes here
 
-setwd("C://Users//spark//OneDrive//Documents//Rwork")
+setwd("C://Users//spark//OneDrive//Documents//Rwork") #change for local machine
 
 NYT_Search_URL <- "https://api.nytimes.com/svc/search/v2/articlesearch.json"
 
@@ -40,42 +26,27 @@ while(page <= maxPages){
                   "facet_filter=","true","&",
                   "facet_fields=", "pub_month", "&",
                   "facet=true","&",
-                  "api-key=uY4GMg9yuDZUZDxq5xdhAhh5BGREoT8E")
+                  "api-key=",yourAPIkey)
   initialQuery <- fromJSON(paste0(basequery,"&page=",page))
   
-  #for testing
-  #facettbl <- initialQuery$response$facets[[1]][[1]]
-  #url_lst <- initialQuery$response$docs$web_url
-  
   #start pasting results into df
-  if(page == 0){ #if first time through, create new df
+  if(page == 0){ #if first time through, create pages list and check max hits / pages
     hits <- initialQuery$response$meta$hits
-    result_df <- data.frame(index=c(1:hits), headline=c(""), url=c(""), pubdate=c(""))
     pages_lst <- list()
     maxPages <- round((hits / 10)-1)
     #save the facet results
     facettbl <- initialQuery$response$facets[[1]][[1]]
-    names(facettbl)[1] <- names(initialQuery$response$facets) #rename term to actual facet var naem
+    names(facettbl)[1] <- names(initialQuery$response$facets) #rename term to actual facet var name
     if(hits>1000){ #only need to check hits the 1st time through
       sprintf("Too many results, hits=%d", hits)
       break
     }
   } 
-  
-  
+    
   #to debug skipping pages
   message("Retrieving page ", page)
-  
-  #page indexing - page = 0 returns results 1-10
-  ## page=1 returns results 11-20
-  ## page=10 returns results 101-110?
-  ## see offset in metareults
-  index <- initialQuery$response$meta$offset
-  result_df[(index+1):(index+10),"headline"] <- initialQuery$response$docs$headline$main
-  result_df[(index+1):(index+10),"url"] <- initialQuery$response$docs$web_url
-  result_df[(index+1):(index+10),"pubdate"] <- initialQuery$response$docs$pub_date
-  
-  #also saving everything to a list for later processing
+    
+  #saving everything to a list for later processing
   pages_lst[[page+1]]<- initialQuery$response$docs
   
   #increment page
@@ -88,7 +59,7 @@ while(page <= maxPages){
 
 #save files for later
 
-#unlist full results - too easy, remove results_df stuff
+#unlist full results
 pages_df <- rbind_pages(pages_lst)
 pages_df <- pages_df %>% mutate(searchstr=searchstr, searchyr=searchyr)
 #need to deal with multimedia and keywords lists at some point
